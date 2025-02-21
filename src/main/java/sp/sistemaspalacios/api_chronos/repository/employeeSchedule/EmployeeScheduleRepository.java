a468dd56b8e75149d3bf187fbb3b9204bc6d7ed3
PROYECTOS_T_I <proyectos_ti@sistemaspalacios.com>
2025-02-21 08:17:50 -0500

    Busqueda por fecha en EmployeeSchedule

--- src/main/java/sp/sistemaspalacios/api_chronos/repository/employeeSchedule/EmployeeScheduleRepository.java
+++ src/main/java/sp/sistemaspalacios/api_chronos/repository/employeeSchedule/EmployeeScheduleRepository.java
@@ -6,16 +6,22 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import sp.sistemaspalacios.api_chronos.entity.employeeSchedule.EmployeeSchedule;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface EmployeeScheduleRepository extends JpaRepository<EmployeeSchedule, Long> {
     List<EmployeeSchedule> findByEmployeeId(Long employeeId);
+
     List<EmployeeSchedule> findByShiftId(Long shiftId);
+
     List<EmployeeSchedule> findByEmployeeIdIn(List<Long> employeeIds);
+
     @Query("SELECT es FROM EmployeeSchedule es JOIN FETCH es.shift WHERE es.id = :id")
     Optional<EmployeeSchedule> findByIdWithShift(@Param("id") Long id);
 
+    @Query("SELECT es FROM EmployeeSchedule es WHERE FUNCTION('DATE', es.startDate) >= :startDate AND FUNCTION('DATE', es.endDate) <= :endDate")
+    List<EmployeeSchedule> findByDateRange(@Param("startDate") Date startDate, @Param("endDate") Date endDate);
 }
 
