fa860eb4bf2ca375cf4918889cddb67481cedb4a
Monita1234 <nayibemontealegre@hotmail.com>
2025-11-15 10:01:14 -0500

    Update of export information

--- src/main/java/sp/sistemaspalacios/api_chronos/ApiChronosApplication.java
+++ src/main/java/sp/sistemaspalacios/api_chronos/ApiChronosApplication.java
@@ -3,6 +3,7 @@ package sp.sistemaspalacios.api_chronos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ApiChronosApplication {
 
